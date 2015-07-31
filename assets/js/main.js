c51df826dc9b3b9f7e143ebb7da5bb17ff921490
Ferdi <falimadhi@gmail.com>
2015-07-30 21:31:58 -0400

    version002

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function($){
+	//open the lateral panel
+	$('.cd-btn').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel').addClass('is-visible');
+	});
+	//close the lateral panel
+	$('.cd-panel').on('click', function(event){
+		if( $(event.target).is('.cd-panel') || $(event.target).is('.cd-panel-close') ) { 
+			$('.cd-panel').removeClass('is-visible');
+			event.preventDefault();
+		}
+	});
+	//open the lateral panel
+	$('.btn-taxonomies').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel-2').addClass('is-visible');
+	});
+	//close the lateral panel
+	$('.cd-panel-2').on('click', function(event){
+		if( $(event.target).is('.cd-panel-2') || $(event.target).is('.cd-panel-close') ) { 
+			$('.cd-panel-2').removeClass('is-visible');
+			event.preventDefault();
+		}
+	});
+	//open the lateral panel
+	$('.btn-export').on('click', function(event){
+		event.preventDefault();
+		$('.cd-panel-3').addClass('is-visible');
+	});
+	//close the lateral panel
+	$('.cd-panel-3').on('click', function(event){
+		if( $(event.target).is('.cd-panel-3') || $(event.target).is('.cd-panel-close') ) { 
+			$('.cd-panel-3').removeClass('is-visible');
+			event.preventDefault();
+		}
+	});
+});
